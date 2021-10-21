5a81cbb8bdb69a08bd99f5250852d8a78a88bbf5
grzeskiewicz <benuch91@gmail.com>
2021-10-22 00:57:17 +0200

    Changed API address

--- src/apiconnection.js
+++ src/apiconnection.js
@@ -1,4 +1,5 @@
-export const API_URL='https://taskmanager-node.herokuapp.com';
+//export const API_URL='https://taskmanager-node.herokuapp.com';
+export const API_URL = 'http://localhost:3001';
 
 export const headers = new Headers({
     'Accept': 'application/json',
